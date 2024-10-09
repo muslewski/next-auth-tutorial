12622b5f68c3bead5b8a0d17cc4342455704dd95
KentoDecem <10kento10@gmail.com>
2024-10-09 18:20:31 +0200

    Update to 1:40:21

--- actions/register.ts
+++ actions/register.ts
@@ -1,13 +1,39 @@
 "use server";
+
+import bcrypt from "bcrypt";
 import * as z from "zod";
+
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-  const validateFields = RegisterSchema.safeParse(values);
+  const validatedFields = RegisterSchema.safeParse(values);
 
-  if (!validateFields.success) {
+  // Check if fields are invalid
+  if (!validatedFields.success) {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  // Check if user exist
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
 };
